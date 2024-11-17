4addb2e60974537a66ed2327074528117f155b13
Martin <Martin2xx@yahoo.com>
2024-11-17 11:11:43 -0600

    Added JavaScript

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-async function searchGiphys () { 
-    const myApiKey = 'NV4Ys85GdAYoR83pCu9Dsk6Bo9H6xnQw';
-    const userSearches = document.queryselector('search-box').value;
-    const searchUrl = `https://api.giphy.com/v1/gifs/search?q=${userSearches}&api_key=${myApiKey}&limit=25`;
-
-    try{
-        const response = await fetch(searchUrl);
-        if(!response.ok) throw new Error('HTTP error!');
-        const data = await response.json();
-        displayGiphys(data.data);
+async function searchGiphys(event) {
+    event.preventDefault();  
+    const myApiKey = "NV4Ys85GdAYoR83pCu9Dsk6Bo9H6xnQw";
+    const userSearches = document.getElementById("search-box").value;
+    const searchUrl = `https://api.giphy.com/v1/gifs/search?q=${userSearches}&api_key=${myApiKey}`;
+  
+    try {
+      const response = await fetch(searchUrl);
+      if (!response.ok) throw new Error("HTTP error!");
+      const data = await response.json();
+      const giphySection = document.getElementById("giphySection");
+      giphySection.innerHTML = ""; 
+  
+      data.data.forEach(element => {
+        const newImages = document.createElement("img");
+        newImages.src = element.images.original.url;
+        giphySection.appendChild(newImages);
+      });
+    } catch (error) {
+      console.error("Error fetching Giphy data:", error); 
     }
-
-
-catch {}
-};
-
-
-searchButton = document.querySelector('.search-box');
-searchButton.addEventListener('click', )
+  }
+  
+  function clearSearch() {
+    document.getElementById("search-box").value = "";
+    const giphySection = document.getElementById("giphySection");
+    giphySection.innerHTML = ""; 
+  }
+  
